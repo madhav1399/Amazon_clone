5dae9b44bf500d0305afd04410e8447ded8f401b
madhav1399 <madhav.manthini@gmail.com>
2024-01-26 19:25:50 +0530

    adding code files (home page is setup)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={[<Header />, <Home />]}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+// Additional component for the /checkout route
+function Checkout() {
+  return (
+    <div>
+      <Header />
+      <h1>Checkout</h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-Q7ZHvvDbCru2nWwrP9oBa3yRBQmbnnM",
+    authDomain: "clone-89644.firebaseapp.com",
+    projectId: "clone-89644",
+    storageBucket: "clone-89644.appspot.com",
+    messagingSenderId: "43250013952",
+    appId: "1:43250013952:web:f34f328569a5712abd05e5"
+  };
